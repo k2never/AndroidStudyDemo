3c2b3ee3b189065d627242a250bb4e197221e927
chenjixuan <shenzi1228@163.com>
2016-05-30 16:35:27 +0800

    Add Android LifeCycle demo 1.Start FirstActivity to see which callback method be called.

--- app/src/main/java/com/k2never/androidstudydemo/MainActivity.java
+++ app/src/main/java/com/k2never/androidstudydemo/MainActivity.java
@@ -1,13 +1,41 @@
 package com.k2never.androidstudydemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.k2never.androidstudydemo.androidlifecycle.FirstActivity;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnActivityLifeCycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnActivityLifeCycle = (Button) findViewById(R.id.button_activity_lifecycle);
+
+        initListen();
+    }
+
+    private void initListen() {
+        mBtnActivityLifeCycle.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_activity_lifecycle:
+                Intent intent = new Intent(this, FirstActivity.class);
+                startActivity(intent);
+                break;
+            default:
+        }
+
     }
 }
---
+++ app/src/main/java/com/k2never/androidstudydemo/androidlifecycle/FirstActivity.java
@@ -0,0 +1,70 @@
+package com.k2never.androidstudydemo.androidlifecycle;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+import com.k2never.androidstudydemo.R;
+
+public class FirstActivity extends AppCompatActivity {
+    public static final String TAG = "AndroidLifeCycle";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Log.i(TAG, "onCreate...The activity is being created.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart...The activity is about to become visible.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume...The activity has become visible (it is now \"resumed\").");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause...Another activity is taking focus (this activity is about to be \"paused\").");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop...The activity is no longer visible (it is now \"stopped\")");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy...The activity is about to be destroyed.");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
+        Log.i(TAG, "onSaveInstanceState...");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i(TAG, "onRestoreInstanceState...");
+    }
+}
