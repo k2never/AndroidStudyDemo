3e73e2fccc078f3dd72cbed2a7b7f6748676e9ba
chenjixuan <shenzi1228@163.com>
2016-05-30 18:17:53 +0800

    Add Android LifeCycle demo 1.Format the log out

--- app/src/main/java/com/k2never/androidstudydemo/androidlifecycle/FirstActivity.java
+++ app/src/main/java/com/k2never/androidstudydemo/androidlifecycle/FirstActivity.java
@@ -16,42 +16,42 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        Log.i(TAG, "onCreate...The activity is being created.");
+        Log.i(TAG, "onCreate...     The activity is being created.");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        Log.i(TAG, "onStart...The activity is about to become visible.");
+        Log.i(TAG, "onStart...      The activity is about to become visible.");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        Log.i(TAG, "onResume...The activity has become visible (it is now \"resumed\").");
+        Log.i(TAG, "onResume...     The activity has become visible (it is now \"resumed\").");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        Log.i(TAG, "onPause...Another activity is taking focus (this activity is about to be \"paused\").");
+        Log.i(TAG, "onPause...      Another activity is taking focus (this activity is about to be \"paused\").");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        Log.i(TAG, "onStop...The activity is no longer visible (it is now \"stopped\")");
+        Log.i(TAG, "onStop...       The activity is no longer visible (it is now \"stopped\")");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        Log.i(TAG, "onDestroy...The activity is about to be destroyed.");
+        Log.i(TAG, "onDestroy...    The activity is about to be destroyed.");
     }
 
     @Override
